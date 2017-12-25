23b6e26bd8a1196f6d53b08293e9b9a1636c49be
David Ke Hong <hongkedavid@gmail.com>
2017-12-24 20:34:48 -0500

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,4 +1,4 @@
-# Ubuntu 14.04 
+# Build dependency for Ubuntu 14.04 
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends \
       build-essential \
@@ -11,6 +11,8 @@ sudo apt-get install -y --no-install-recommends \
       python-pip                          
 sudo pip install numpy protobuf
 
+# Build and install Caffe2
 git clone --recursive https://github.com/caffe2/caffe2.git
-cd caffe2
-make
+cd caffe2 && make
+cd build && sudo make install
+python -c 'from caffe2.python import core' 2>/dev/null && echo "Success" || echo "Failure"
