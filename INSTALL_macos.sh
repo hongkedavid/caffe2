099177f2a54a9ceb480ad92308690421389af5a2
David Ke Hong <hongkedavid@gmail.com>
2017-12-27 00:43:36 -0500

    Update INSTALL_macos.sh

--- INSTALL_macos.sh
+++ INSTALL_macos.sh
@@ -14,6 +14,7 @@ numpy \
 protobuf=3.4.0 \
 six
 
+# Build Caffe2
 git clone --recursive https://github.com/caffe2/caffe2.git && cd caffe2
 mkdir build && cd build
 cmake -Wno-dev -DUSE_CUDA=OFF ..
@@ -21,3 +22,9 @@ sudo make install
 
 # If make install fails, try 
 brew install protobuf
+
+# Run Caffe2
+python -c 'from caffe2.python import core' 2>/dev/null && echo "Success" || echo "Failure"
+
+# To enable Caffe2 outside build/, make sure the following command is run or added to .bashrc or .bash_profile
+export PYTHONPATH=/usr/local
